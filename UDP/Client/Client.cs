9fff0ff470963013dfc911f292d4e822312c9182
mathiasnielsen0 <47353101+mathiasnielsen0@users.noreply.github.com>
2019-03-30 22:32:31 +0100

    .proj upload

---
+++ UDP/Client/Client.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    public class Client
+    {
+        //Stuff for client
+        private const int PORT = 9000;
+        private Boolean done;
+
+        private Socket sending_socket;
+        private Boolean exception_thrown;
+        private IPAddress send_to_address;
+        private IPEndPoint sending_end_point;
+
+        //Stuff to receive
+        private string received_data;
+        private byte[] receive_byte_array;
+        private UdpClient listener;
+        private IPEndPoint groupEP;
+
+        
+
+        public Client()
+        {
+            sending_socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            done = false;
+            exception_thrown = false;
+            send_to_address = IPAddress.Parse("10.0.0.2"); 
+            sending_end_point = new IPEndPoint(send_to_address, PORT);
+
+            //Stuff to receive
+            listener = new UdpClient(PORT);
+            groupEP = new IPEndPoint(IPAddress.Any, PORT);
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Write a text to the server. 'u' for uptime and 'l' for loadavg.");
+            Console.WriteLine("Enter a blank line to exit the program.");
+            while (!done)
+            {
+                Console.WriteLine("Enter text to send, blank line to quit");
+                string text_to_send = Console.ReadLine();
+                if (text_to_send.Length == 0)
+                {
+                    done = true;
+                }
+                else
+                {
+                    byte[] send_buffer = Encoding.ASCII.GetBytes(text_to_send);
+
+                    Console.WriteLine("sending to address: {0} port: {1}",
+                        sending_end_point.Address,
+                        sending_end_point.Port);
+                    try
+                    {
+                        sending_socket.SendTo(send_buffer, sending_end_point);
+                    }
+                    catch (Exception send_exception)
+                    {
+                        exception_thrown = true;
+                        Console.WriteLine(" Exception {0}", send_exception.Message);
+                    }
+
+                    if (exception_thrown == false)
+                    {
+                        Console.WriteLine("Message has been sent to the broadcast address");
+                        if (text_to_send == "u" || text_to_send == "U" || text_to_send == "l" || text_to_send == "L")
+                        {
+                            Console.WriteLine("Waiting for response from server");
+                            ReceiveMsg();
+                        }
+                    }
+                    else
+                    {
+                        exception_thrown = false;
+                        Console.WriteLine("The exception indicates the message was not sent.");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        private void ReceiveMsg()
+        {
+            receive_byte_array = listener.Receive(ref groupEP);
+            Console.WriteLine("Received a broadcast from {0}", groupEP.ToString());
+            received_data = Encoding.ASCII.GetString(receive_byte_array, 0, receive_byte_array.Length);
+            Console.WriteLine("Data: \n{0}\n\n", received_data);
+        }
+    }
+}
+
---
+++ UDP/UDP/Server.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UDP
+{
+    public class Server
+    {
+
+
+        private const int PORT = 9000;
+
+        //Stuff to receive
+        private bool done;
+        private string received_data;
+        private byte[] receive_byte_array;
+        private UdpClient listener;
+        private IPEndPoint groupEP;
+
+        
+        //Stuff to send
+        private Socket sending_socket;
+        private Boolean exception_thrown;
+        private IPAddress send_to_address;
+        private IPEndPoint sending_end_point;
+
+        public Server()
+        {
+            done = false;
+            listener = new UdpClient(PORT);
+            groupEP = new IPEndPoint(IPAddress.Any, PORT);
+
+            //Stuff to send
+            sending_socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
+                ProtocolType.Udp);
+            done = false;
+            exception_thrown = false;
+            send_to_address = IPAddress.Parse("10.0.0.1");
+            sending_end_point = new IPEndPoint(send_to_address, PORT);
+        }
+
+        public void Start()
+        {
+            try
+            {
+
+                while (!done)
+                {
+                    Console.WriteLine("Waiting for broadcast");
+                    
+                    // Blocking call
+                    receive_byte_array = listener.Receive(ref groupEP);
+                    Console.WriteLine("Received a broadcast from {0}", groupEP.ToString());
+                    received_data = Encoding.ASCII.GetString(receive_byte_array, 0, receive_byte_array.Length);
+                    Console.WriteLine("data follows \n{0}\n\n", received_data);
+
+                    switch (received_data)
+                    {
+                        case "u":
+                        {
+                            SendMsg(UpTime());
+                        }
+                        break;
+
+                        case "U":
+                        {   
+                            SendMsg(UpTime());
+                        }
+                        break;
+
+                        case "l":
+                        {
+                            SendMsg(AvgLoad());
+                        }
+                        break;
+
+                        case "L":
+                        {
+                            SendMsg(AvgLoad());
+                        }
+                        break;
+
+                        default:
+                        {
+                                Console.WriteLine("Data didn't match any commands");
+                        }
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+           listener.Close();
+        }
+
+        private void SendMsg(string text_to_send)
+        {
+            byte[] send_buffer = Encoding.ASCII.GetBytes(text_to_send);
+
+            // Remind the user of where this is going.
+            Console.WriteLine("sending to address: {0} port: {1}",
+                sending_end_point.Address,
+                sending_end_point.Port);
+            try
+            {
+                sending_socket.SendTo(send_buffer, sending_end_point);
+            }
+            catch (Exception send_exception)
+            {
+                exception_thrown = true;
+                Console.WriteLine(" Exception {0}", send_exception.Message);
+            }
+
+            if (exception_thrown == false)
+            {
+                Console.WriteLine("Message has been sent to the broadcast address");
+            }
+            else
+            {
+                exception_thrown = false;
+                Console.WriteLine("The exception indicates the message was not sent.");
+            }
+        }
+
+        private string AvgLoad()
+        {
+            string text;
+
+            using (StreamReader sr = new StreamReader("/~/../proc/avgload"))
+            {
+                text = sr.ReadToEnd();
+            }
+
+            return text;
+        }
+
+        private string UpTime()
+        {
+            string text;
+
+            using (StreamReader sr = new StreamReader("/~/../proc/uptime"))
+            {
+                text = sr.ReadToEnd();
+            }
+            
+            return text;
+        }
+    }
+}
